d7cbce7e325fa2f03c26194a3a4385e71628f743
Choiyounho <maxosa78@gmail.com>
2021-09-22 18:44:48 +0900

    [feat] : My page

--- app/src/main/java/com/soten/genieforum/ui/main/settings/SettingsFragment.kt
+++ app/src/main/java/com/soten/genieforum/ui/main/settings/SettingsFragment.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.soten.genieforum.R
 import com.soten.genieforum.databinding.FragmentSettingsBinding
+import com.soten.genieforum.di.ForumModule.provideFirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment() {
 
-    private lateinit var settingsViewModel: SettingsViewModel
     private var _binding: FragmentSettingsBinding? = null
-
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,17 +22,17 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
-
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textNotifications
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.logoutButton.setOnClickListener {
+            provideFirebaseAuth().signOut()
+            requireActivity().finish()
+        }
     }
 
     override fun onDestroyView() {
